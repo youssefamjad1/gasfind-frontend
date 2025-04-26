8b810cdcbea9773e212bd3af00a738619d8b9aba
Youssef HIMMI <yhimmi1@gmail.com>
2025-04-26 20:06:02 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'GasFind',
         short_name: 'GasFind',
